2db44a95a6244c0199b4c83658e9bfec50e8b35d
Mexus <gilaldpellaeon@gmail.com>
2016-01-18 20:06:14 +0300

    Read data from command line arguments

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ A tool to get info from c++ code and to refactor it using [clang](http://clang.l
 
 ##Plan
 - [ ] Retrieve information about a symbol for specified file:column.
+  - [x] Basic information
+  - [ ] Full information
 - [ ] Get all the files from a compilation database.
 - [ ] Use `grep` and [LibTooling](http://clang.llvm.org/docs/LibTooling.html)'s
-capabilities to find all instances of a specified symbol.
+capabilities to find all instances of a specified symbol...
 - [ ] ... and to rename it.
--- tool/main.cpp
+++ tool/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <boost/filesystem.hpp>
 #include "compilation_db.h"
 #include "cursor.h"
 #include "diagnostic.h"
@@ -27,12 +28,49 @@ std::string SeverityToString(CXDiagnosticSeverity severity) {
 }
 }
 
-int main(int /*argc*/, char** /*argv*/) {
-    bool display_diagnostic = true;
-    int exclude_declarations_from_pch = 0;
+std::string NormalizePath(const std::string& file) {
+    using namespace boost::filesystem;
+    return canonical(absolute(file)).string();
+}
+
+bool CheckCompileCommands(const std::string& path_to_compile_commands) {
+    using namespace boost::filesystem;
+    path cc_path(path_to_compile_commands);
+    if (!exists(cc_path)) {
+        std::cerr << "Path to `compile_commands.json` doesn't exist\n";
+        return false;
+    }
+    if (!exists(cc_path / "compile_commands.json")) {
+        std::cerr << "`compile_commands.json` doesn't exist in `" << cc_path << "`\n";
+        return false;
+    }
+    return true;
+}
+
+void ChangeCurrentPath(const std::string& new_path) {
+    boost::filesystem::current_path(new_path);
+}
+
+int main(int argc, char** argv) {
+    if (argc < 5) {
+        std::cerr << argv[0] << " <compile_commands.json dir> <file to parse> <line> <column>\n";
+        return 1;
+    }
+
+    const bool display_diagnostic = true;
+    const int exclude_declarations_from_pch = 0;
+    const std::string path_to_compile_commands = NormalizePath(argv[1]);
+    const std::string file_name = NormalizePath(argv[2]);
+    const unsigned line = std::stoul(argv[3]);
+    const unsigned number = std::stoul(argv[4]);
+
+    if (!CheckCompileCommands(path_to_compile_commands)) {
+        return 1;
+    }
+    ChangeCurrentPath(path_to_compile_commands);
+
     cx::Index index(exclude_declarations_from_pch, display_diagnostic);
-    cx::CompilationDatabase comp_db("/home/mexus/c++11-projects/codeinfo-tool/build/");
-    const std::string file_name = "/home/mexus/c++11-projects/codeinfo-tool/tool/src/temp_file.cpp";
+    cx::CompilationDatabase comp_db(path_to_compile_commands);
     cx::TranslationUnit unit(index, file_name, comp_db);
 
     for (unsigned i = 0, n = unit.getNumDiagnostics(); i != n; ++i) {
@@ -46,24 +84,24 @@ int main(int /*argc*/, char** /*argv*/) {
     std::cout << "File parsed OK\n";
 
     cx::File file(unit, file_name);
-    cx::SourceLocation location(unit, file, 11, 32);
+    cx::SourceLocation location(unit, file, line, number);
     cx::Cursor cursor(unit, location);
 
     if (cursor.isNull()) {
-        std::cout << "Cursor is null\n";
-        return 0;
+        std::cerr << "Cursor is null\n";
+        return 1;
     }
 
-    unsigned line, column, offset;
-    location.getFileLocation(&file, &line, &column, &offset);
+    unsigned effective_line, effective_column, effective_offset;
+    location.getFileLocation(&file, &effective_line, &effective_column, &effective_offset);
     std::cout << "Location: ";
     if (file) {
         std::cout << file.getFileName();
     } else {
         std::cout << "[no file]";
     }
-    std::cout << ", line " << line << ", "
-              << "column " << column << ", offset " << offset << "\n";
+    std::cout << ", line " << effective_line << ", "
+              << "column " << effective_column << ", offset " << effective_offset << "\n";
 
     std::cout << "Location is from main file? " << std::boolalpha << location.isFromMainFile() << "\n";
     std::cout << "Cursor kind: " << cursor.getCursorKind() << "\n";
